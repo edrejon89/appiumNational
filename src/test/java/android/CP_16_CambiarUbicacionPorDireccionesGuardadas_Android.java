046a91aebc373a2a99b5f10c839ae4304b5a2914
Eduardo Rejon <erejon@walook.com.mx>
2018-09-03 11:06:09 -0500

    Se carga cp17 con gitginore

---
+++ src/test/java/android/CP_16_CambiarUbicacionPorDireccionesGuardadas_Android.java
@@ -0,0 +1,57 @@
+package android;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CP_16_CambiarUbicacionPorDireccionesGuardadas_Android extends BaseTest{
+
+    @Test
+    public void cambiarUbicacionPorDireccionesGuardadas() {
+        try {
+            String[] parametros = leerCSV("src/test/java/configuracion/CP16_Configuracion");
+            Thread.sleep(8000);
+            MobileElement btnMenu = (MobileElement) driver.findElementByAccessibilityId("Open");
+            btnMenu.click();
+            MobileElement btnIniciarSesion = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.support.v7.widget.LinearLayoutCompat[2]");
+            btnIniciarSesion.click();
+            MobileElement txtEmail = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/txtEmail")));
+            txtEmail.sendKeys(parametros[0]);
+            MobileElement txtPassword = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/txtpassword")));
+            txtPassword.sendKeys(parametros[1]);
+            MobileElement btnLogin = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/btnlogin")));
+            btnLogin.click();
+            MobileElement btnLugaresCercanos = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.ImageView")));
+            btnLugaresCercanos.click();
+            Thread.sleep(1500);
+            MobileElement btnDireccion = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/lblLocation")));
+            String direccion = btnDireccion.getText();
+            btnDireccion.click();
+            MobileElement btnDireccionesGuardadas = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/btnOption1")));
+            btnDireccionesGuardadas.click();
+            Thread.sleep(3000);
+            MobileElement btnListo = (MobileElement) driver.findElementByAccessibilityId("Listo");
+            btnListo.click();
+            MobileElement btnDireccion2 = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/lblLocation")));
+            String nuevaDireccion = btnDireccion2.getText();
+            Thread.sleep(2000);
+            MobileElement btnRegresar = (MobileElement) driver.findElementByAccessibilityId("Navegar hacia arriba");
+            btnRegresar.click();
+            Thread.sleep(5000);
+            MobileElement btnMenu2 = (MobileElement) driver.findElementByAccessibilityId("Open");
+            btnMenu2.click();
+            MobileElement btnCerrarSesion = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.support.v7.widget.LinearLayoutCompat[7]")));
+            btnCerrarSesion.click();
+            MobileElement btnConfirmar = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/button1")));
+            btnConfirmar.click();
+            Assert.assertNotEquals(direccion, nuevaDireccion);
+        }catch (Exception e){
+            Assert.fail(e.getMessage());
+        }
+    }
+
+}
+
--- src/test/java/android/CP_9_AnadirDirecciones_Android.java
+++ src/test/java/android/CP_9_AnadirDirecciones_Android.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class CP_9_AnadirDirecciones extends BaseTest{
+public class CP_9_AnadirDirecciones_Android extends BaseTest{
 
 
 
