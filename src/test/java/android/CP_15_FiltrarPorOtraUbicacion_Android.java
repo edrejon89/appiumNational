d5fc46f360c0b95624b496ac8a852a22d88a6c0b
Rosi <rosigarcilazo@gmail.com>
2018-09-05 11:27:14 -0500

    correccion cp15

--- src/test/java/android/CP_15_FiltrarPorOtraUbicacion_Android.java
+++ src/test/java/android/CP_15_FiltrarPorOtraUbicacion_Android.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class CP_15_FiltrarPorOtraUbicacion_Android extends BaseTestMac{
+public class CP_15_FiltrarPorOtraUbicacion_Android extends BaseTest{
 
     @Test
     public void filtrarPorrOtraUbicacion() {
@@ -21,7 +21,8 @@ public class CP_15_FiltrarPorOtraUbicacion_Android extends BaseTestMac{
             MobileElement btnElegirDirMapa = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.nationalsoft.srapp:id/btnOption2")));
             btnElegirDirMapa.click();
             Thread.sleep(3000);
-            (new TouchAction(driver)).tap(529, 887).perform();
+            MobileElement btnLugarCercano = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]")));
+            btnLugarCercano.click();
             Thread.sleep(1000);
             MobileElement btnConfirmar = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.google.android.gms:id/confirm_button")));
             btnConfirmar.click();
