35266db09f00b48990f582c07b903c552638ff44
Eduardo Rejon <erejon@walook.com.mx>
2018-09-05 10:21:43 -0500

    Correccion de CP14

--- src/test/java/android/CP_14_FiltarPorCategoria_Android.java
+++ src/test/java/android/CP_14_FiltarPorCategoria_Android.java
@@ -6,37 +6,23 @@ import org.junit.Test;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import sun.jvm.hotspot.debugger.ThreadAccess;
 
 public class CP_14_FiltarPorCategoria_Android extends  BaseTestMac{
     @Test
     public void CP_14_FiltarPorCategoria_AndroidTest(){
 
         try {
-            Thread.sleep(5000);
-////            (new TouchAction(driver))
-////                    .press(437, 1606)
-////                     .moveTo(430, 1196)
-////                     .release()
-////                    .perform();
-////            (new TouchAction(driver))
-////                    .press( 480, 1715)
-////                    .moveTo(499, 937)
-////                    .release()
-////                    .perform();
-//            MobileElement el1 = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[2]/android.widget.ImageView")));
-//            el1.click();
-//            Thread.sleep(1000);
-//            driver.navigate().back();
-//            Thread.sleep(1000);
-//            driver.navigate().back();
-//            MobileElement el2 = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/button1")));
-//            el2.click();
+            Thread.sleep(8000);
+
             MobileElement el1 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[2]/android.widget.ImageView");
             el1.click();
+            Thread.sleep(8000);
+
             driver.navigate().back();
+            Thread.sleep(1000);
             driver.navigate().back();
-            driver.navigate().back();
-            driver.navigate().back();
+            Thread.sleep(4000);
             MobileElement el2 = (MobileElement) driver.findElementById("android:id/button1");
             el2.click();
 
