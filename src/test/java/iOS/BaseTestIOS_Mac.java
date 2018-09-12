ea0ea676eb6ab3109ebcd0b9cd54f2c9c09da240
Eduardo Rejon <erejon@walook.com.mx>
2018-09-12 17:33:03 -0500

    Cp3 falta validar

--- src/test/java/iOS/BaseTestIOS_Mac.java
+++ src/test/java/iOS/BaseTestIOS_Mac.java
@@ -7,7 +7,12 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
@@ -19,6 +24,8 @@ public class BaseTestIOS_Mac {
     private String testName = "Untitled";
     protected IOSDriver<IOSElement> driver = null;
     DesiredCapabilities dc = new DesiredCapabilities();
+    protected WebDriverWait wait;
+    protected String[] parametros;
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -29,9 +36,23 @@ public class BaseTestIOS_Mac {
         dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.softrestaurant.softrestaurantappdistrib");
         driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"), dc);
         driver.setLogLevel(Level.INFO);
+        wait =  new WebDriverWait(driver,60);
     }
+
     @After
     public void tearDown() {
         driver.quit();
     }
+
+    public String[] leerCSV(String path) throws IOException {
+        File f = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line = br.readLine();
+        return line.split(",");
+    }
+
+
+//    public void scrollAndClick(String visibleText) {
+//        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\""+visibleText+"\").instance(0))").click();
+//    }
 }
