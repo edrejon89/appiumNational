e2affbe709ceab26885b37876afddd89bd91bd87
Eduardo Rejon <erejon@walook.com.mx>
2018-09-13 00:06:25 -0500

    CP9 no finializado

---
+++ src/test/java/iOS/CP_9_AnadirDirecciones_IOS.java
@@ -0,0 +1,89 @@
+package iOS;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.IOException;
+
+public class CP_9_AnadirDirecciones_IOS extends BaseTestIOS_Mac{
+    @Test
+    public void CP_9_AnadirDirecciones_IOS(){
+        try {
+            Thread.sleep(15000);
+            parametros = leerCSV("src/test/java/configuracion/CP9_Configuracion");
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Ingresar']")));
+            String fallo = driver.findElement(By.xpath("//*[@text='Ingresar']")).getText();
+
+            if (fallo.equals("Ingresar")) {
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Ingresar']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder=' correo@ejemplo.com']"))).sendKeys(parametros[0]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder=' **********']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder=' **********']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder=' **********']"))).sendKeys(parametros[1]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='INICIAR SESIÓN']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='INICIAR SESIÓN']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Mis direcciones']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Direcciones']/*[@class='UIAButton' and @width>0])[2]"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Identificador (Ej. Casa, Oficina)']"))).sendKeys(parametros[2]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Calle']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Calle']"))).sendKeys(parametros[3]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Cruzamientos']"))).click();
+                driver.executeScript("seetest:client.deviceAction(\"BKSP\")");
+                driver.getKeyboard().sendKeys("25");
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).sendKeys(parametros[4]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='C.P.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='C.P.']"))).sendKeys(parametros[5]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']"))).sendKeys(parametros[6]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GUARDAR']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GUARDAR']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Cerrar sesión']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
+            }else{
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Mis direcciones']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Direcciones']/*[@class='UIAButton' and @width>0])[2]"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Identificador (Ej. Casa, Oficina)']"))).sendKeys(parametros[2]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Calle']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Calle']"))).sendKeys(parametros[3]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Cruzamientos']"))).click();
+                driver.executeScript("seetest:client.deviceAction(\"BKSP\")");
+                driver.getKeyboard().sendKeys("25");
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Número Ext.']"))).sendKeys(parametros[4]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='C.P.']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='C.P.']"))).sendKeys(parametros[5]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='more, numbers']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Referencias']"))).sendKeys(parametros[6]);
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GUARDAR']")));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GUARDAR']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Cerrar sesión']"))).click();
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
+            }
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+
+    }
+}
