9fb0b31fe65f7e08d44cdf4bfe40c6c29547017f
Eduardo Rejon <erejon@walook.com.mx>
2018-09-17 16:41:46 -0500

    CP 3

--- src/test/java/iOS/CP_3_CreacionCuentaFB_Mac.java
+++ src/test/java/iOS/CP_3_CreacionCuentaFB_Mac.java
@@ -14,27 +14,15 @@ public class CP_3_CreacionCuentaFB_Mac extends BaseTestIOS_Mac {
         try {
             Thread.sleep(15000);
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Crear cuenta']")));
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Crear cuenta']"))).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Continue with Facebook']")));
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Continue with Facebook']"))).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Continuar']")));
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Continuar']"))).click();
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@placeholder='Fecha de Nacimiento']")));
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Fecha de Nacimiento']"))).click();
-            new TouchAction(driver).press(574, 750).moveTo(586, 1025).release().perform();
-            new TouchAction(driver).press(574, 750).moveTo(586, 1025).release().perform();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='1987']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='ic check circle']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Género']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='- Tu genero -']"))).click();
-            new TouchAction(driver).press(574, 750).moveTo(586, 650).release().perform();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Femenino']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='ic check circle']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Móvil']"))).sendKeys("9999999999");
-            driver.hideKeyboard();
-            Thread.sleep(2000);
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GUARDAR']"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='OMITIR']"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='OMITIR']"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Cerrar sesión']"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
+
         } catch (Exception e) {
             Assert.fail(e.getMessage());
         }
