c74846a99a78f709bbde28b72b03e79f59f83df4
Eduardo Rejon <erejon@walook.com.mx>
2018-09-17 17:17:01 -0500

    cp8

--- src/test/java/iOS/CP_8_IniciarSesionFb_IOS.java
+++ src/test/java/iOS/CP_8_IniciarSesionFb_IOS.java
@@ -16,20 +16,10 @@ public class CP_8_IniciarSesionFb_IOS extends BaseTestIOS_Mac{
            Thread.sleep(15000);
            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Ingresar']"))).click();
-           new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Continue with Facebook']")));
            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Continue with Facebook']"))).click();
-           new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Continuar']")));
            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Continuar']"))).click();
-           new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Aceptar']")));
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Continue with Facebook']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='UIAWebView']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='UIAWebView']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='principal']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='principal']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='OK']"))).click();
-           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='UIAWindow' and ./*[@class='UIAStatusBar']]"))).click();
-           new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Aceptar']")));
+           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
+           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Cerrar sesión']"))).click();
            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
        } catch (Exception e) {
            Assert.fail(e.getMessage());
