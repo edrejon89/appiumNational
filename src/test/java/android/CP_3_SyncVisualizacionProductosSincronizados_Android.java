3c0fa0d888c2a876c4d28903b149ea4c39686a14
Rosi <rosigarcilazo@gmail.com>
2018-09-24 10:37:54 -0500

    creacion cp3sync

---
+++ src/test/java/android/CP_3_SyncVisualizacionProductosSincronizados_Android.java
@@ -0,0 +1,11 @@
+package android;
+
+import org.junit.Test;
+
+public class CP_3_SyncVisualizacionProductosSincronizados_Android  extends BaseTest{
+
+    @Test
+    public void visualizacionProductosSincronizados(){
+
+    }
+}
