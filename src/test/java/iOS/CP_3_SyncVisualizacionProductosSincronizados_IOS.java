f288b46db20589637e0bfd7975d5ac4e58d11ad8
Eduardo Rejon <erejon@walook.com.mx>
2018-09-25 11:46:53 -0500

    Sync

--- src/test/java/iOS/CP_3_SyncVisualizacionProductosSincronizados_IOS.java
+++ src/test/java/iOS/CP_3_SyncVisualizacionProductosSincronizados_IOS.java
@@ -8,23 +8,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-<<<<<<< HEAD
-=======
+
 import java.util.List;
 
->>>>>>> d290e052e0c64b5b5da9db1256a5ad83107f238f
+
 public class CP_3_SyncVisualizacionProductosSincronizados_IOS extends BaseTestIOS_Win {
 
     @Test
     public void CP_3_SyncVisualizacionProductosSincronizados_IOSTest(){
 
         try {
-<<<<<<< HEAD
 
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[2]"))).click();
 
 
-=======
             parametros = leerCSV("src/test/java/configuracion/CP3_Sincronizador");
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[1]"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Ingresar']"))).click();
@@ -34,7 +31,6 @@ public class CP_3_SyncVisualizacionProductosSincronizados_IOS extends BaseTestIO
 
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Categorias']/*[@class='UIAButton'])[2]"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Restaurante...']"))).click();
->>>>>>> d290e052e0c64b5b5da9db1256a5ad83107f238f
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@placeholder='Restaurante...']"))).sendKeys("Walook");
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Buscar']"))).click();
             Thread.sleep(5000);
@@ -42,14 +38,12 @@ public class CP_3_SyncVisualizacionProductosSincronizados_IOS extends BaseTestIO
             IOSElement restaurant = (IOSElement) rest.get(2);
             restaurant.click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='  ORDENAR DEL MENU']"))).click();
-<<<<<<< HEAD
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Pasar a recoger']"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='GIOCONDA LUNCH $77']")));
 //            IOSElement elem = driver.findElement(By.xpath("//*[@text='GIOCONDA LUNCH $77']"));
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("((//*[@class='UIATable' and ./parent::*[./parent::*[./parent::*[@class='UIAView']]]]/*[@class='UIAView'])[1]/*[@text='ic keyboard backspace'])[2]"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("((//*[@class='UIATable' and ./parent::*[./parent::*[./parent::*[@class='UIAView']]]]/*[@class='UIAView'])[1]/*[@text='ic keyboard backspace'])[2]"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@text='Restaurantes']/*[@class='UIAButton'])[1]"))).click();
-=======
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='A domicilio']"))).click();
             Thread.sleep(5000);
             List paquetes = driver.findElements(By.xpath("//*[@name='More Info']"));
@@ -66,7 +60,6 @@ public class CP_3_SyncVisualizacionProductosSincronizados_IOS extends BaseTestIO
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Cerrar sesión']"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Aceptar']"))).click();
 
->>>>>>> d290e052e0c64b5b5da9db1256a5ad83107f238f
         } catch (Exception e) {
             Assert.fail(e.getMessage());
         }
